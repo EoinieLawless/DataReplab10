2686ee6d653817e07904a4c8d8286a84f533e85f
unknown <martin.kenirons@gmit.ie>
2022-10-24 09:37:14 +0000

    lab complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,10 +1,82 @@
 import React from "react";
 
-export class Create extends React.Component{
-    render(){
-        return(
+export class Create extends React.Component {
+
+    constructor(){
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeBookTitle = this.onChangeBookTitle.bind(this);
+        this.onChangeBookCover = this.onChangeBookCover.bind(this);
+        this.onChangeBookAuthor = this.onChangeBookAuthor.bind(this);
+        this.state = {
+            title:'',
+            cover:'',
+            author:''
+        }
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        console.log(`Button clicked 
+        ${this.state.title},
+        ${this.state.cover},
+        ${this.state.author}`);
+        this.setState({
+            title:'',
+            cover:'',
+            author:''
+        })
+    }
+
+    onChangeBookTitle(e){
+        this.setState({
+            title:e.target.value
+        })
+    }
+    onChangeBookCover(e){
+        this.setState({
+            cover:e.target.value
+        })
+    }
+    onChangeBookAuthor(e){
+        this.setState({
+            author:e.target.value
+        })
+    }
+
+    render() {
+        return (
             <div>
                 <h3>Hello from Create Component!</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Add Book Title: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.title}
+                            onChange={this.onChangeBookTitle}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Book Cover: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.cover}
+                            onChange={this.onChangeBookCover}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Author: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.author}
+                            onChange={this.onChangeBookAuthor}
+                        />
+                    </div>
+
+                    <input type="submit" value="Add Book" />
+                </form>
             </div>
         );
     }
--- src/components/read.js
+++ src/components/read.js
@@ -1,44 +1,22 @@
 import React from "react";
 import { Books } from "./books";
+import axios from "axios";
 
 export class Read extends React.Component{
     
+
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/1027219693823606784')
+        .then((response)=>{
+            this.setState({books:response.data})
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
     state = {
-        books:[
-            {
-            "title": "Learn Git in a Month of Lunches",
-            "isbn": "1617292419",
-            "pageCount": 0,
-            "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg",
-            "status": "MEAP",
-            "authors": ["Rick Umali"],
-            "categories": []
-            },
-            {
-            "title": "MongoDB in Action, Second Edition",
-            "isbn": "1617291609",
-            "pageCount": 0,
-            "thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-            "status": "MEAP",
-            "authors": [
-            "Kyle Banker",
-            "Peter Bakkum",
-            "Tim Hawkins",
-            "Shaun Verch",
-            "Douglas Garrett"
-            ],
-            "categories": []
-            },
-            {
-            "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-            "isbn": "1617292036",
-            "pageCount": 0,
-            "thumbnailUrl":"https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-            "status": "MEAP",
-            "authors": ["Simon Holmes"],
-            "categories": []
-            }
-            ]
+        books:[ ]
     }
     
     render(){
